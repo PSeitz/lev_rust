5bd348644190a20df120e5ddfd3ec271a8e6b1ff
Pascal Seitz <pascal.seitz@gmail.com>
2019-01-22 23:26:50 +0100

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ impl LevenshteinAutomaton {
         row
     }
     pub fn step(&self, state:&LevenshteinAutomatonState, cha:char) -> LevenshteinAutomatonState {
-        let mut new_state = vec![state[0]+1];
+        let mut new_state = vec![(state[0]+1).min(self.max_edits+1)];
         println!("{:?}", state);
         for (i, val) in state.iter().take(state.len()-1).enumerate() {
             let cost = if self.string.chars().nth(i).map(|c|c == cha).unwrap_or(false) {
@@ -33,16 +33,16 @@ impl LevenshteinAutomaton {
             };
             let candidates = &[new_state[i]+1, val + cost, state[i + 1] + 1];
             let yop = candidates.iter().min().unwrap();
-            new_state.push(*yop);
+            new_state.push(*yop.min(&(self.max_edits+1)));
         }
 
         new_state
     }
     pub fn is_match(&self, state:&LevenshteinAutomatonState) -> bool {
-        false
+        state.last().unwrap() > &self.max_edits
     }
     pub fn can_match(&self, state:&LevenshteinAutomatonState) -> bool {
-        false
+        state.iter().any(|el| el<=&self.max_edits)
     }
 }
 
@@ -55,10 +55,14 @@ mod tests {
         let automaton = LevenshteinAutomaton::new("banana", 1);
         let start = automaton.start();
         let s1 = automaton.step(&start, 'w');
+        println!("{:?}", s1);
         println!("{:?}", automaton.can_match(&s1));
-        let s2 = automaton.step(&s1, 'o');
-        println!("{:?}", automaton.can_match(&s2));
-
+        let s1 = automaton.step(&s1, 'o');
+        let s1 = automaton.step(&s1, 'o');
+        let s1 = automaton.step(&s1, 'o');
+        let s1 = automaton.step(&s1, 'o');
+        println!("{:?}", s1);
+        println!("{:?}", automaton.can_match(&s1));
     }
 }
 
